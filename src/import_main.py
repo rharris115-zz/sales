93564a3854fa3b29d4d59667de44a3e41ccf51c6
rich <richard.w.harris@mac.com>
2020-05-21 06:34:35 +0100

    main entrypoint and packaging

--- src/import_main.py
+++ src/import_main.py
@@ -9,22 +9,22 @@ from sqlalchemy.orm import sessionmaker, Session
 
 
 @click.command()
-@click.argument('date', type=click.DateTime())
+@click.argument('business-date', type=click.DateTime(formats=['%Y-%m-%d']))
 @click.argument('output', type=click.Path(resolve_path=True))
-@click.option('--products', '-p', type=click.File('r'), default='product_data.json')
-@click.option('--stores', '-s', type=click.File('r'), default='store_data.json')
-@click.option('--sales-1', '-s1', type=click.File('r'), default='sales_one_data.json')
-@click.option('--sales-2', '-s2', type=click.File('r'), default='sales_two_data.csv')
-def main(date: datetime, output: str,
+@click.option('--products', '-p', type=click.File('r'), default='product_data.json', help='The products file.')
+@click.option('--stores', '-s', type=click.File('r'), default='store_data.json', help='The stores file.')
+@click.option('--sales-1', '-s1', type=click.File('r'), default='sales_one_data.json', help='The sales one file.')
+@click.option('--sales-2', '-s2', type=click.File('r'), default='sales_two_data.csv', help='The sales two file.')
+def main(business_date: datetime, output: str,
          products: TextIOWrapper, stores: TextIOWrapper,
          sales_1: TextIOWrapper, sales_2: TextIOWrapper):
-    business_date = date.date()
+    business_date = business_date.date()  # We don't need the time component.
 
-    path = 'sqlite:///' + output
+    path = 'sqlite:///' + output  # output will be an absolute path.
     engine = create_engine(path)
     create_tables(engine=engine)
 
-    session = sessionmaker(bind=engine)()
+    session: Session = sessionmaker(bind=engine)()
 
     # Import SKUs and prices for the date.
     import_products(date=business_date, products=products, session=session)
